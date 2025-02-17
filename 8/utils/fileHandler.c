0d4f39d3712808f25994480d55f183805c4a6999
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2025-02-17 17:10:17 +0300

    Added fileInput func

---
+++ 8/utils/fileHandler.c
@@ -0,0 +1,37 @@
+#include <fileHandler.h>
+
+
+char *setFilename(int argc, char *argv[]){
+    if (argc != 2 || strpbrk("./\\\\<>:", argv[1])){
+        fprintf(stderr, "Provide correct filename");
+        return NULL;
+    }
+    return strstr(argv[1], ".bin") == NULL ? strcat(argv[1], ".bin") : argv[1];
+}
+
+
+void fileInput(char *filename){
+    FILE *file = fopen(filename, "wb+");
+    Elem *len;
+    int range[2] = {0, INT_MAX};
+    do {
+        if ((len = input("Enter the number of elements: ")) != NULL && len->type == typeInt && (range == NULL || (len->value.Int > range[0] && len->value.Int < range[1])))
+            break;
+        printf("Wrong input, try again\n");
+    } while (true);
+    Elem *elem;
+    for (int i = 0; i < len->value.Int; i++)
+        do {
+            if ((elem = input("Enter the element [%d]: ", i)) != NULL && elem->type == typeInt){
+                fwrite(&(elem->value.Int), sizeof(int), 1, file);
+                break;
+            }
+            printf("Wrong input, try again\n");
+        } while (true);
+    rewind(file);
+    while (fread(&(elem->value.Int), sizeof(int), 1, file))
+        printf("%d ", elem->value.Int);
+    printf("\n");
+    fclose(file);
+}
+
