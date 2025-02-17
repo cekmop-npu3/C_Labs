172dec2407811e8889d54c3b0746b37645367a98
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2025-02-17 17:10:41 +0300

    Added fileInput func

---
+++ 8/utils/fileHandler.h
@@ -0,0 +1,12 @@
+#ifndef FILE_HANDLER
+#define FILE_HANDLER
+
+#include <inputHandler.h>
+#include <stdio.h>
+#include <stdint.h>
+
+char *setFilename(int argc, char *argv[]);
+
+void fileInput(char *filename);
+
+#endif
