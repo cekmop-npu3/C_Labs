dacedcc5f54e23fbe9159c2d2265c814fe66f6ab
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2025-02-24 19:46:44 +0300

    Added swap function

--- 9/src/3.c
+++ 9/src/3.c
@@ -1,56 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include <fileHandler.h>
 
 
-typedef bool (*Lambda)(char *, char *);
+typedef bool (*Lambda)(int *bound1, int *bound2);
 
 
-bool greaterThan(char *str1, char *str2){
-    return strlen(str1) > (str2 == NULL ? 0 : strlen(str2));
+bool greaterThan(int bound1[2], int bound2[2]){
+    return (bound1[1] - bound1[0]) > (bound2[1] - bound2[0]);
 }
 
-bool lessThan(char *str1, char *str2){
-    return (str2 == NULL ? true : strlen(str1) < strlen(str2));
+bool lessThan(int bound1[2], int bound2[2]){
+    return (!bound2[0] && !bound2[1] ? true : (bound1[1] - bound1[0]) < (bound2[1] - bound2[0]));
 }
 
 
-void getWord(char *filename, Lambda callable){
+int *getWord(char *filename, Lambda callable){
     FILE *file = fopen(filename, "r");
     char c;
-    char *prev = NULL;
-    char *word = NULL;
     bool inWord = true;
     int size = 0;
+    int *prev = malloc(sizeof(int) * 2);
+    prev[0] = 0;
+    prev[1] = 0;
+    int word[2] = {0, 0};
     while ((c = getc(file)) != EOF){
         c = (c == '\t' ? ' ' : c);
         if (c != ' '){
-            if (inWord && size){
-                word = realloc(word, ++size);
-                word[size - 1] = '\0';
+            if (inWord && (word[1] - word[0])){
                 if (callable(word, prev))
-                    prev = realloc(word, size);
-                word = NULL;
-                size = 0;
+                    memcpy(prev, word, sizeof(int) * 2);
+                word[0] = word[1];
             }
-            word = realloc(word, ++size);
-            word[size - 1] = c;
             inWord = false;
         }
         else
             inWord = true;
+        word[1]++;
     }
-    word = realloc(word, ++size);
-    word[size - 1] = '\0';
-    if (callable(word, prev))
-        prev = realloc(word, size);
-    printf("%s", prev);
-    free(prev);
-    free(word);
+    if (callable(word, prev)){
+        memcpy(prev, word, sizeof(int) * 2);
+    }
+    else
+        --prev[1];
+    fclose(file);
+    return prev;
 }
 
+
+void swapWords(char *filename, int *bound1, int *bound2){
+    FILE *file = fopen(filename, "r+");
+    int len1 = bound1[1] - bound1[0];
+    int len2 = bound2[1] - bound2[0];
+    int index = 0;
+    for (index; index < (len1 > len2 ? len2 : len1); index++)
+        fileSwap(file, bound1[0] + index, bound2[0] + index);
+    int ranged[2] = {bound2[0] > bound1[0] ? bound1[0] + index: bound2[0] + index, bound2[0] > bound1[0] ? bound2[1] - 1: bound1[1] - 1};
+    printf("%d %d", ranged[0], ranged[1]);
+    for (int i = 0; i < (len1 < len2 ? len2 : len1) - index; i++)
+        shiftFile(file, ((len1 < len2) && (bound1[0] < bound2[0]) ? shiftRight : shiftLeft), ranged);
+    fclose(file);
+}
 
 
 int main(int argc, char *argv[]){
@@ -58,7 +71,11 @@ int main(int argc, char *argv[]){
     if (filename == NULL)
         return EXIT_FAILURE;
     fileInput(filename);
-    getWord(filename, lessThan);
+    int *shortStr = getWord(filename, lessThan);
+    int *longStr = getWord(filename, greaterThan);
+    swapWords(filename, shortStr, longStr);
+    free(shortStr);
+    free(longStr);
     return EXIT_SUCCESS;
 }
 
