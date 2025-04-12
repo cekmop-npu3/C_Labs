b3efbd30feff111fffb1f673420a8913e5cf8b54
cekmop-npu3 <igor.serkov1005@gmail.com>
2025-04-12 17:10:14 +0300

    test

--- 11/src/main.c
+++ 11/src/main.c
@@ -1,14 +1,10 @@
 #include <eval.h>
+#include <input.h>
   
 
 int main(){
-    const char exp[] = "(-12+23)*5";
-    if (hasError(exp))
-        return EXIT_FAILURE;
-    Deque *postfix = infixToPostfix(exp);
-    Item *item = evalPostfix(postfix);
-    printf("%.2f\n", *(double *) item->data);
-    freeItem(item);
-    freeDeque(postfix);
+    Elem *elem = handle(typeDouble, NULL, "Some message: ");
+    printf("%d\n", elem->type);
+    freeElem(elem);
     return EXIT_SUCCESS;
 }
